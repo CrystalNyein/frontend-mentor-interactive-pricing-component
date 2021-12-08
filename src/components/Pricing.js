3b0f38f0066cb6b053be1989ec12e2dac15de77a
CrystalNyein <nyeinchanlay99@gmail.com>
2021-12-08 21:01:30 +0630

    Html added

---
+++ src/components/Pricing.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+const Pricing = () => {
+  return (
+    <div className="Pricing">
+      <div className="price-req">
+        <p>Pageviews</p>
+        <p>
+          $ <span>/month</span>
+        </p>
+      </div>
+      <div className="slide">
+        <input type="range" min="0" max="200" value="100" />
+      </div>
+      <div className="billing">
+        <p>Monthly Billing</p>
+        <button></button>
+        <p>Yearly Billing</p>
+        <p>25% discount</p>
+      </div>
+      <hr></hr>
+      <div className="trial">
+        <ul className="benefits">
+          <li>Unlimited websites</li>
+          <li>100% data ownership</li>
+          <li>Email reports</li>
+        </ul>
+        <button className="trial-btn">Start my trial</button>
+      </div>
+    </div>
+  );
+};
+
+export default Pricing;
