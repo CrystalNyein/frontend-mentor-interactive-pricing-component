559118b7b96e46c92911f610c5181a5b02040e36
CrystalNyein <nyeinchanlay99@gmail.com>
2021-12-08 12:21:43 +0630

    App html done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Pricing from "./components/Pricing";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <main className="App">
+      <h1>Simple, traffic-based pricing </h1>
+      <h2>Sign-up for our 30-day trial. No credit card required.</h2>
+      <Pricing />
+    </main>
+  );
 }
 
 export default App;
